9509992916380d826a21231af5f4288300bbfc53
easychatroom <luis.ferreira.web.4008@gmail.com>
2025-06-02 07:25:13 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBhfQ1Wf5JEy6sOU4ExXboRI4Ir4y_aKZw",
   authDomain: "easy-chatroom.firebaseapp.com",
@@ -17,7 +9,6 @@ const firebaseConfig = {
   measurementId: "G-Q0YZGZLEMM"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 firebase.initializeApp(firebaseConfig);
